00c1ab06b77b098f6ed222b0faff21964b5d893c
João Pedro <jpedro1580@gmail.com>
2021-04-15 04:11:23 -0300

    Fallback engine for compileLatex function

--- lua/simple-latex/init.lua
+++ lua/simple-latex/init.lua
@@ -1,5 +1,8 @@
 vim.g.simple_latex_default_mappings = true
 
+if (vim.g.simple_latex_engines ==  nil) then
+	vim.g.simple_latex_engines = {"pdflatex"}
+end
 if (vim.g.simple_latex_default_mappings) then
 	require('simple-latex.keymaps')
 end
