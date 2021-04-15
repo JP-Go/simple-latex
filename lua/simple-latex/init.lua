d3d0edf503097d9570df4b3443227b43ddb1b9bb
João Pedro <jpedro1580@gmail.com>
2021-04-15 03:56:34 -0300

    Fix typo

--- lua/simple-latex/init.lua
+++ lua/simple-latex/init.lua
@@ -4,7 +4,7 @@ if (vim.g.simple_latex_engines == nil) then
 	vim.g.simple_latex_engines = {"pdflatex","xelatex","lualatex"}
 end
 if (vim.g.simple_latex_viewer == nil) then
-	vim.g.simple_latex_viewers = "xdg-open"
+	vim.g.simple_latex_viewer = "xdg-open"
 end
 if (vim.g.simple_latex_default_mappings) then
 	require('simple-latex.keymaps')
