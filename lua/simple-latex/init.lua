3754c8f060930dbb2d3f49459636a923d1aea292
João Pedro <jpedro1580@gmail.com>
2021-04-15 03:54:48 -0300

    Adding of fallback values for plugin variables if not user set

--- lua/simple-latex/init.lua
+++ lua/simple-latex/init.lua
@@ -1,9 +1,9 @@
 vim.g.simple_latex_default_mappings = true
 
-if (vim.g.simple_latex_engines == "") then
+if (vim.g.simple_latex_engines == nil) then
 	vim.g.simple_latex_engines = {"pdflatex","xelatex","lualatex"}
 end
-if (vim.g.simple_latex_viewer == "") then
+if (vim.g.simple_latex_viewer == nil) then
 	vim.g.simple_latex_viewers = "xdg-open"
 end
 if (vim.g.simple_latex_default_mappings) then
