656208fb270426cc566f40821ba466ec6fd1ae68
João Pedro <jpedro1580@gmail.com>
2021-05-05 18:04:16 -0300

    keymaps.lua: Add a keymap to view the logfile --/--: Change the viewpdf command name and keymap

--- lua/simple-latex/keymaps.lua
+++ lua/simple-latex/keymaps.lua
@@ -3,4 +3,5 @@ local opts = {noremap = true}
 keymap('n','<space>cp','<cmd> CompileLatex pdflatex<CR>',opts)
 keymap('n','<space>cx','<cmd> CompileLatex xelatex<CR>',opts)
 keymap('n','<space>cl','<cmd> CompileLatex lualatex<CR>',opts)
-keymap('n','<space>pp','<cmd> PreviewPdf<CR>',{noremap =  true, silent = true})
+keymap('n','<space>vp','<cmd> ViewPdf<CR>',{noremap =  true, silent = true})
+keymap('n','<space>vl','<cmd> ViewLaTeXLog<CR>',{noremap =  true, silent = true})
