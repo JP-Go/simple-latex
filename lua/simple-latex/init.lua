3d5c3dbb35c59119b95c1f778e6cfb002737d3a2
João Pedro <jpedro1580@gmail.com>
2021-07-14 11:27:19 -0300

    Initialize simple-latex without options

--- lua/simple-latex/init.lua
+++ lua/simple-latex/init.lua
@@ -13,14 +13,15 @@ local M = {}
 M.default_mappings = vim.g.simple_latex_default_mappings or true
 M.engines = vim.g.simple_latex_engines or {'pdflatex'}
 M.setup = function(options)
-    require('simple-latex.commands')
     local options = options or nil
     if options then
         M.default_mappings = options.default_mappings or M.default_mappings 
         M.engines = options.engines or M.engines
-        if M.default_mappings then
-            require('simple-latex.keymaps')
-        end
+    end
+
+    require('simple-latex.commands')
+    if M.default_mappings then
+        require('simple-latex.keymaps')
     end
 end
 
