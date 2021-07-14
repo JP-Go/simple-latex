7794d582133845fb46aa0aea7cdc6257a9b747b4
João Pedro <jpedro1580@gmail.com>
2021-07-14 14:23:08 -0300

    typo

--- lua/simple-latex/init.lua
+++ lua/simple-latex/init.lua
@@ -5,7 +5,7 @@ M.setup = function(options)
     local options = options or false
     opts.mergeOptions(options)
 
-    if M.opts.default_mappings then
+    if opts.default_mappings then
         require('simple-latex.keymaps')
     end
     require('simple-latex.commands')
