91111d83ff73d4a0f7cb3ef07fe1107aefe7225c
João Pedro <jpedro1580@gmail.com>
2021-07-14 15:33:39 -0300

    Introduce fallback option reading

--- lua/simple-latex/init.lua
+++ lua/simple-latex/init.lua
@@ -3,8 +3,9 @@ local opts= require('simple-latex.options')
 
 M.setup = function(options)
     local options = options or false
-    opts.mergeOptions(options)
-
+    if (options) then
+        opts.mergeOptions(options)
+    end
     if opts.default_mappings then
         require('simple-latex.keymaps')
     end
