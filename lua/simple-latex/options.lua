8d4608abe90fe88e875e51c64e6350658f10c766
João Pedro <jpedro1580@gmail.com>
2021-07-14 15:00:45 -0300

    Typo

--- lua/simple-latex/options.lua
+++ lua/simple-latex/options.lua
@@ -15,13 +15,14 @@ M.opts = {
     synctex = userVars.synctex or false,
     compile_options = userVars.compile_options or false,
 }
+
 M.mergeOptions = function(new)
     if new then
         M.opts.default_mappings = M.default_mappings and (new.default_mappings or userVars.default_mappings) 
         M.opts.engines = new.engines or M.opts.engines
         M.opts.viewer = new.viewer or M.opts.viewer
         M.opts.synctex = new.synctex or M.opts.synctex
-        M.opts.compile_options = options.compile_options or M.opts.compile_options
+        M.opts.compile_options = new.compile_options or M.opts.compile_options
     end
 end
 
