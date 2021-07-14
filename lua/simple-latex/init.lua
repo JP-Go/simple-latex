26e7e98326e199d002bfdfba6ec2d560b22292d4
João Pedro <jpedro1580@gmail.com>
2021-07-14 10:47:43 -0300

    Modularize simple-latex

--- lua/simple-latex/init.lua
+++ lua/simple-latex/init.lua
@@ -1,9 +1,24 @@
-vim.g.simple_latex_default_mappings = true
+local M = {}
 
-if (vim.g.simple_latex_engines ==  nil) then
-	vim.g.simple_latex_engines = {"pdflatex"}
+-- vim.g.simple_latex_default_mappings = true
+-- 
+-- if (vim.g.simple_latex_engines ==  nil) then
+-- 	vim.g.simple_latex_engines = {"pdflatex"}
+-- end
+-- if (vim.g.simple_latex_default_mappings ~= false or vim.g.simple_latex_default_mappings == nil) then
+-- 	require('simple-latex.keymaps')
+-- end
+-- require('simple-latex.commands')
+
+M.default_mappings = vim.g.simple_latex_default_mappings or true
+M.engines = vim.g.simple_latex_engines or {'pdflatex'}
+M.setup = function(options)
+    require('simple-latex.commands')
+    M.default_mappings = options.default_mappings or M.default_mappings 
+    M.engines = options.engines or M.engines
+    if M.default_mappings then
+        require('simple-latex.keymaps')
+    end
 end
-if (vim.g.simple_latex_default_mappings ~= false or vim.g.simple_latex_default_mappings == nil) then
-	require('simple-latex.keymaps')
-end
-require('simple-latex.commands')
+
+return M
