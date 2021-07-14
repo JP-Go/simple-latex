12b34902eda97aa74cf34187e6f0bbbb4f28ffdf
João Pedro <jpedro1580@gmail.com>
2021-07-14 11:23:45 -0300

    Fix

--- lua/simple-latex/init.lua
+++ lua/simple-latex/init.lua
@@ -14,10 +14,13 @@ M.default_mappings = vim.g.simple_latex_default_mappings or true
 M.engines = vim.g.simple_latex_engines or {'pdflatex'}
 M.setup = function(options)
     require('simple-latex.commands')
-    M.default_mappings = options.default_mappings or M.default_mappings 
-    M.engines = options.engines or M.engines
-    if M.default_mappings then
-        require('simple-latex.keymaps')
+    local options = options or nil
+    if options then
+        M.default_mappings = options.default_mappings or M.default_mappings 
+        M.engines = options.engines or M.engines
+        if M.default_mappings then
+            require('simple-latex.keymaps')
+        end
     end
 end
 
